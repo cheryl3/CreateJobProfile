e380e36050b843c9932c8f2a6a47450ae002f516
cheryl3 <chermer01@gmail.com>
2016-03-19 13:16:06 +0530

    Create Job profile 2nd commit primary key issues

--- src/main/java/org/crce/interns/model/Profile.java
+++ src/main/java/org/crce/interns/model/Profile.java
@@ -3,21 +3,28 @@ package org.crce.interns.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SecondaryTable;
 import javax.persistence.SecondaryTables;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.ForeignKey;
+
+
 @Entity
 @Table(name = "job_schema.job")
-@SecondaryTables({
-		@SecondaryTable(name = "job_schema.criteria", pkJoinColumns = @PrimaryKeyJoinColumn(name = "criteria_id") ),
-		 })
+
 public class Profile implements Serializable {
 
 	private static final long serialVersionUID = -3153101241508852277L;
@@ -62,10 +69,172 @@ public class Profile implements Serializable {
 	@Column(name="modified_by")
 	private String modified_by;
 	
-	//@Id
-	/*@OneToOne  
+	private Criteria criteria;
+	
+	public Profile() {
+		criteria = new Criteria();
+    }   
+	
+	public String getCompany_id() {
+		return company_id;
+	}
+	public void setCompany_id(String company_id) {
+		this.company_id = company_id;
+	}
+	public String getJob_id() {
+		return job_id;
+	}
+	public void setJob_id(String job_id) {
+		this.job_id = job_id;
+	}
+	public String getEvent_id() {
+		return event_id;
+	}
+	public void setEvent_id(String event_id) {
+		this.event_id = event_id;
+	}
+	public String getJob_description() {
+		return job_description;
+	}
+	public void setJob_description(String job_description) {
+		this.job_description = job_description;
+	}
+	public String getJob_category() {
+		return job_category;
+	}
+	public void setJob_category(String job_category) {
+		this.job_category = job_category;
+	}
+	public String getCtc() {
+		return ctc;
+	}
+	public void setCtc(String ctc) {
+		this.ctc = ctc;
+	}
+	public String getSkills_required() {
+		return skills_required;
+	}
+	public void setSkills_required(String skills_required) {
+		this.skills_required = skills_required;
+	}
+	public String getDocs_required() {
+		return docs_required;
+	}
+	public void setDocs_required(String docs_required) {
+		this.docs_required = docs_required;
+	}
+	public Date getDrive_date() {
+		return drive_date;
+	}
+	public void setDrive_date(Date drive_date) {
+		this.drive_date = drive_date;
+	}
+	public Date getCreated_date() {
+		return created_date;
+	}
+	public void setCreated_date(Date created_date) {
+		this.created_date = created_date;
+	}
+	public String getCreated_by() {
+		return created_by;
+	}
+	public void setCreated_by(String created_by) {
+		this.created_by = created_by;
+	}
+	public Date getModified_date() {
+		return modified_date;
+	}
+	public void setModified_date(Date modified_date) {
+		this.modified_date = modified_date;
+	}
+	public String getModified_by() {
+		return modified_by;
+	}
+	public void setModified_by(String modified_by) {
+		this.modified_by = modified_by;
+	}
+	
+	@OneToOne(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+    //@JoinColumn(name="company_id",insertable=false,updatable=false)
+	@JoinTable(name="job_schema.criteria",
+    joinColumns = {@JoinColumn(name="criteria_id", referencedColumnName="criteria_id")}
+    /*inverseJoinColumns = {@JoinColumn(name="role_id", referencedColumnName="id")}*/
+)
+	public Criteria getCriteria() {
+		return criteria;
+	}
+	/*
+	public void setCriteria(Criteria criteria) {
+		this.criteria = criteria;
+	}
+	
+	*/	
+	
+}
+
+
+/*
+@Entity
+@Table(name = "job_schema.job")
+@SecondaryTables({
+		@SecondaryTable(name = "job_schema.criteria", pkJoinColumns = @PrimaryKeyJoinColumn(name = "criteria_id") ),
+		 })
+public class Profile implements Serializable {
+
+	private static final long serialVersionUID = -3153101241508852277L;
+
+	@JoinColumn(name="company_id")
+	private String company_id;
+	
+	@Id
+	@Column(name="job_id")
+	private String job_id;
+	
+	@Column(name="event_id")
+	private String event_id;
+	
+	@Column(name="job_description")
+	private String job_description;
+	
+	@Column(name="job_category")
+	private String job_category;
+	
+	@Column(name="ctc")
+	private String ctc;
+	
+	@Column(name="skills_required")
+	private String skills_required;
+	
+	@Column(name="docs_required")
+	private String docs_required;
+	
+	@Column(name="drive_date")
+	private Date drive_date;
+	
+	@Column(name="created_date")
+	private Date created_date;
+	
+	@Column(name="created_by")
+	private String created_by;
+	
+	@Column(name="modified_date")
+	private Date modified_date;
+	
+	@Column(name="modified_by")
+	private String modified_by;
+	
+	/*@Id
     @Join*/
-    @Column(name="criteria_id",table="job_schema.criteria",insertable = false, updatable = false)
+	// /////// @Column(name="criteria_id",table="job_schema.criteria",insertable = false, updatable = false)
+	
+	/*@OneToOne(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+    @JoinColumn(name="PASS_ID",insertable=true,
+        updatable=true,nullable=true,unique=true)*/
+	
+	/*@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")*/
+	
+	/*/////////////////////
 	private String criteria_id;
 	
 	@Column(name="eligible_branches",table="job_schema.criteria")
@@ -256,3 +425,4 @@ public class Profile implements Serializable {
 	
 	
 }
+////////////////////  */
